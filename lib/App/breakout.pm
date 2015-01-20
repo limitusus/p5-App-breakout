fe7e971c709239d7831e367ec936fd377343434d
limitusus <limit.usus@gmail.com>
2015-01-20 11:31:52 +0900

    module

---
+++ lib/App/breakout.pm
@@ -0,0 +1,39 @@
+package App::breakout;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::breakout - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::breakout;
+
+=head1 DESCRIPTION
+
+App::breakout is
+
+=head1 AUTHOR
+
+Tomoya KABE E<lt>limitusus@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Tomoya KABE
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
